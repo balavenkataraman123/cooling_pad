d2e53d7d052137a61eb93040493608970229cbde
Bala Venkataraman <38150148+balavenkataraman123@users.noreply.github.com>
2023-08-07 12:55:38 -0400

    Update faninator.ino

--- faninator.ino
+++ faninator.ino
@@ -1,6 +1,8 @@
 
 int fan_relay_pin = 11;
 int override_switch_pin = 10;
+int heat_indicator_pin = 9;
+int override_indicator_pin = 8;
 
 
 bool fan_state = false;
@@ -13,7 +15,10 @@ void setup()
 
 {
 pinMode(fan_relay_pin, OUTPUT);
-pinMode(override_switch_pin, OUTPUT);
+pinMode(heat_indicator_pin, OUTPUT);
+pinMode(override_indicator_pin, OUTPUT);
+
+pinMode(override_switch_pin, INPUT);
 Serial.begin(9600);
 }
 
@@ -36,13 +41,25 @@ else{
   } 
 }
 button_state = digitalRead(override_switch_pin);
-if(button_state == HIGH){
+if(button_state == HIGH){ // this line needs to be changed when using toggle instead of momentary switch
   switch_override += 1;
   switch_override = switch_override % 2;
 }
 
-if( (fan_state == true && ghosted_counter < 50 ) || switch_override == 1){
-  digitalWrite(fan_relay_pin, HIGH);
+if(switch_override == 1){
+    digitalWrite(override_indicator_pin, HIGH);
+}
+else{
+  digitalWrite(override_indicator_pin, LOW);
+}
+if(fan_state == true && ghosted_counter < 50){
+    digitalWrite(heat_indicator_pin, HIGH);
+}
+else{
+  digitalWrite(heat_indicator_pin, LOW);
+}
+if((fan_state == true && ghosted_counter < 50) || switch_override == 1){ 
+  digitalWrite(fan_relay_pin, HIGH); 
 }
 else{
   digitalWrite(fan_relay_pin, LOW);
